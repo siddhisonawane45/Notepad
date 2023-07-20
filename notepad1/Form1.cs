b718adeb53e79a40dc640310d8065b1d284f7224
siddhisonawane45 <siddhisonawane02@gmail.com>
2023-07-20 16:00:43 +0530

    my project

---
+++ notepad1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog siddhi = new OpenFileDialog();
+            siddhi.Title = "Open";
+            siddhi.Filter = "Text Document(*.txt)|*.txt|All Files (*.*)|*.*";
+            if (siddhi.ShowDialog() == DialogResult.OK)
+                richTextBox1.LoadFile(siddhi.FileName, RichTextBoxStreamType.PlainText);
+            this.Text = siddhi.FileName;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog siddhi = new SaveFileDialog();
+            siddhi.Title = "Save";
+            siddhi.Filter = "Text Document(*.txt)|*.txt|All Files (*.*)|*.*";
+            if (siddhi.ShowDialog() == DialogResult.OK)
+                richTextBox1.LoadFile(siddhi.FileName, RichTextBoxStreamType.PlainText);
+            this.Text = siddhi.FileName;
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dateTimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = System.DateTime.Now.ToString();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fnt = new FontDialog();
+            if(fnt.ShowDialog() == DialogResult.OK);
+
+            richTextBox1.Font = fnt.Font;
+
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog fnt = new ColorDialog();
+            if (fnt.ShowDialog() == DialogResult.OK) ;
+
+            richTextBox1.ForeColor = fnt.Color;
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
